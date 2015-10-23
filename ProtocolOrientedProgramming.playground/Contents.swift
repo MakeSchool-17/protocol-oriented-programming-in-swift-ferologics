092ef8a4adcf1a4d4e7a2c79f1600363f1393b80
ferologics <f.hetes@gmail.com>
2015-10-22 22:38:43 -0700

    Not sure why but the extension doesn't make conform to protocol which doesn't make sense to me.. Can you review it Benji?

--- ProtocolOrientedProgramming.playground/Contents.swift
+++ ProtocolOrientedProgramming.playground/Contents.swift
@@ -3,115 +3,89 @@
 import Cocoa
 
 /**
-Vehicles can have different properties and functionality.
-
-All Vehicles:
-  - Have a speed at which they move
-  - Calculate the duration it will take them to travel a certain distance
-
-All Vehicles except a Motorcycle and a Container Ship:
-  - Have an amount of Windows
-
-Only Ground Vehicles:
-  - Have an amount of wheels
-
-Only Buses:
-  - Have a seating capacity
-
-Only Vessels:
-  - Have a length
-
-Create the following Vehicles types: Car, Bus, ContainerShip, Boat, Motorcycle
-
-----
-
-The solution below does not use protocols, it relies solely on subclassing. Can you use protocols to improve the solution?
-**/
+ Vehicles can have different properties and functionality.
+ 
+ All Vehicles:
+ - Have a speed at which they move
+ - Calculate the duration it will take them to travel a certain distance
+ 
+ All Vehicles except a Motorcycle and a Container Ship:
+ - Have an amount of Windows
+ 
+ Only Ground Vehicles:
+ - Have an amount of wheels
+ 
+ Only Buses:
+ - Have a seating capacity
+ 
+ Only Vessels:
+ - Have a length
+ 
+ Create the following Vehicles types: Car, Bus, ContainerShip, Boat, Motorcycle
+ 
+ ----
+ 
+ The solution below does not use protocols, it relies solely on subclassing. Can you use protocols to improve the solution?
+ **/
 
 typealias KilometersPerHour = Double
 typealias Kilometers = Double
 typealias Hours = Double
 
-class Vehicle {
-  
-  var speed: KilometersPerHour = 0
-  
-  func travelDuration(distance: Kilometers) -> Hours {
-    return distance / speed
-  }
-  
+protocol VehicleType
+{
+    var speed: KilometersPerHour { get }
+    func travelDuration(distance: Kilometers?) -> Double
+}
+protocol GroundVehicleType { var wheels: Int { get } }
+protocol BusType           { var seats:  Int { get } }
+protocol VesselType        { var length: Int { get } }
+
+enum Vehicles
+{
+    case Car          (wheels:  Int, windows: Int)
+    case Motocycle    (wheels:  Int, windows: Int)
+    case Bus          (wheels:  Int, windows: Int, seats: Int)
+    case Vessel       (length:  Int, windows: Int)
+    case ContainerShip(length:  Int)
 }
 
-class GroundVehicle: Vehicle {
-  
-  var wheels: Int = 0
-  
+extension VehicleType
+{
+    func travelDuration(distance: Kilometers = 100) -> Double
+    {
+        return ( distance / speed )
+    }
 }
 
-class Car: GroundVehicle {
-  
-  var amountOfWindows: Int = 0
-  
+struct Car: GroundVehicleType, VehicleType
+{
+    var windows = 4
+    var wheels  = 4
+    var speed   = 70
 }
 
-class MotorCycle: GroundVehicle {
-  
+struct MotorCycle: GroundVehicleType, VehicleType
+{
+    var wheels = 2
+    var speed  = 90
 }
 
-class Bus: GroundVehicle {
-  
-  var capacity: Int = 0
-  var amountOfWindows: Int = 0
-
+struct Bus: GroundVehicleType, BusType, VehicleType
+{
+    var wheels  = 4
+    var seats   = 80
+    var windows = 20
 }
 
-class Vessel: Vehicle {
-  
-  var length: Int = 0
-  
+struct Boat: VehicleType, VesselType
+{
+    var windows = 0
+    var speed   = 45
 }
 
-class Boat: Vessel {
-  
-  var amountOfWindows: Int = 0
-  
-}
-
-class ContainerShip: Vessel {
-  
-}
-
-var car = Car()
-car.amountOfWindows = 6
-car.speed = 120
-
-var bus = Bus()
-bus.amountOfWindows = 20
-bus.speed = 100
-
-var boat = Boat()
-boat.amountOfWindows = 8
-boat.speed = 30
-
-var containerShip = ContainerShip()
-containerShip.speed = 30
-
-var motorcycle = MotorCycle()
-motorcycle.speed = 130
-
-let vehicles = [car, bus, boat, containerShip, motorcycle]
-
-for v in vehicles {
-  var amountOfWindowsSubstring = ""
-  
-  if let boat = v as? Boat {
-      amountOfWindowsSubstring = " has \(boat.amountOfWindows) windows and"
-  } else if let car = v as? Car {
-      amountOfWindowsSubstring = " has \(car.amountOfWindows) windows and"
-  } else if let bus = v as? Bus {
-    amountOfWindowsSubstring = " has \(bus.amountOfWindows) windows and"
-  }
-
-  
-  print("\(v.dynamicType)\(amountOfWindowsSubstring) needs \(v.travelDuration(100)) to travel 100 kilometers.")
+struct ContainerShip: VehicleType, VesselType
+{
+    var length = 5
+    var speed  = 45
 }
